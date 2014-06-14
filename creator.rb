dc52670581988101ff348e4f6e49c18b33df567d
Harrison Brown (MacBook Pro) <harrisonbro@gmail.com>
2014-06-14 19:48:47 +0100

    Getting workspace names to choose from...

--- creator.rb
+++ creator.rb
@@ -1,10 +1,16 @@
+# Dependencies
 require 'json'
 require 'Asana'
 
+# Configuration
 config = JSON.parse(File.read('config.json'))
 
 Asana.configure do |client|
   client.api_key = config['api_key']
 end
 
-puts Asana::User.me
+# Ask user to choose a workspace
+workspaces = Asana::Workspace.all
+workspaces.each { |workspace|
+    p workspace.name
+}

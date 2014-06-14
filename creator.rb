fa4b4c5c1ee0ac82cb2b86c2324eae16221c9bd6
Harrison Brown (iMac) <harrisonbro@gmail.com>
2014-06-14 22:34:32 +0100

    Got workspace from user

--- creator.rb
+++ creator.rb
@@ -11,11 +11,11 @@ end
 
 # Ask user to choose a workspace
 workspaces = Asana::Workspace.all
-puts "Which workspace should this project be created in?"
-puts "  (type your chosen number, then press enter)"
+puts "Which workspace should this project be created in?\n\n"
 workspaces.each_with_index { |workspace, index|
     puts "  [#{index}] #{workspace.name}"
 }
+puts "\n→ Type your choice and press enter..."
 workspace_index = gets
 workspace = workspaces[workspace_index.to_i]
-puts "OK, creating project in the #{workspace.name} workspace"
+puts "\nOK, creating project in the #{workspace.name} workspace"

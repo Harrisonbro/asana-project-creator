fe5a057a67ce8fe7095b2625609f68d4104fbae6
Harrison Brown (MacBook Pro) <harrisonbro@gmail.com>
2014-06-16 08:06:42 +0100

    Moving to yaml config and adding templating

--- creator.rb
+++ creator.rb
@@ -1,47 +1,83 @@
 #
 # Dependencies
 #
-require 'json'
+require 'yaml'
 require 'Asana'
 
+# #
+# # Configuration
+# #
+# config = YAML.load_file('config.yaml')
+
+# Asana.configure do |client|
+#   client.api_key = config['api_key']
+# end
+
+# puts "Talking to Asana..."
+
+# #
+# # Ask user to choose a workspace
+# #
+# workspaces = Asana::Workspace.all
+
+# puts "Which workspace should this project be created in?\n\n"
+
+# def ask_for_workspace(workspaces)
+#     workspaces.each_with_index { |workspace, index|
+#         puts "  [#{index}] #{workspace.name} (id: #{workspace.id})"
+#     }
+
+#     puts "\n→ Type your choice and press enter..."
+
+#     workspace_index = Integer(gets) rescue -1
+
+#     if workspace_index < 0 || workspaces[workspace_index].nil?
+#         puts "Your answer wasn't valid. Please choose from the following options:\n\n"
+#         ask_for_workspace(workspaces)
+#     else
+#         workspace = workspaces[workspace_index]
+#         return workspace
+#     end
+# end
+
+# workspace = ask_for_workspace(workspaces)
+
+# puts workspace.id
+
+# puts "\nOK, creating project in the #{workspace.name} workspace"
+
 #
-# Configuration
+# Ask user to choose template
 #
-config = JSON.parse(File.read('config.json'))
 
-Asana.configure do |client|
-  client.api_key = config['api_key']
-end
+templates = Hash.new
+Dir.glob("templates/**.yaml") { |entry|
+    template = YAML.load_file(entry)
+    templates[template['template_name']] = template
+}
 
-puts "Talking to Asana..."
+puts "Which project template do you want to use?\n\n"
 
-#
-# Ask user to choose a workspace
-#
-workspaces = Asana::Workspace.all
-
-puts "Which workspace should this project be created in?\n\n"
-
-def ask_for_workspace(workspaces)
-    workspaces.each_with_index { |workspace, index|
-        puts "  [#{index}] #{workspace.name} (id: #{workspace.id})"
+def ask_for_template(templates)
+    templates.each_with_index { |template, index|
+        puts "  [#{index}] #{template.template_name}"
     }
 
     puts "\n→ Type your choice and press enter..."
 
-    workspace_index = Integer(gets) rescue -1
+    template_index = Integer(gets) rescue -1
 
-    if workspaces[workspace_index].nil?
+    if template_index < 0 || templates[template_index].nil?
         puts "Your answer wasn't valid. Please choose from the following options:\n\n"
-        ask_for_workspace(workspaces)
+        ask_for_workspace(templates)
     else
-        workspace = workspaces[workspace_index]
-        return workspace
+        template = templates[template_index]
+        return template
     end
 end
 
-workspace = ask_for_workspace(workspaces)
+template = ask_for_template(templates)
 
-puts workspace.id
+puts template.id
 
-puts "\nOK, creating project in the #{workspace.name} workspace"
+puts "\nOK, creating a new project from #{template.template_name}"
